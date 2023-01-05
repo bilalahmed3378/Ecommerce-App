76c3d330e127131de39d73718b408908a7d1ab91
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-01-05 10:48:46 +0500

    Initial Commit

---
+++ Ecommerce App/Ecommerce_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecommerce_AppApp.swift
+//  Ecommerce App
+//
+//  Created by Bilal Ahmed on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ecommerce_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
